44d80c8b3be0201269cffdee3a821c0473bf5939
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2026-08-05 08:31:15 +0300

    Scanner flow

--- src/scanner.rs
+++ src/scanner.rs
@@ -19,7 +19,7 @@ impl<T: ContentType> Scanner<T> {
         }
         self.inner_scan(content, 0);
     }
-    fn inner_scan(&mut self, content: &mut dyn Content<T>, depth: u32) {
+    fn inner_scan(&mut self, content: &mut dyn Content<T>, depth: u32) -> NextAction {
         let ty = self.retrieve_content_type(content);
         let range = if let Some(ty) = ty {
             self.analyzers.range(ty)
@@ -27,56 +27,94 @@ impl<T: ContentType> Scanner<T> {
             None
         };
         if let Some((start, end)) = range {
-            self.scan_range(content, start, end, depth);
+            match self.scan_range(content, start, end) {
+                NextAction::Continue => {},
+                NextAction::Skip => return NextAction::Continue, // skip current content
+                NextAction::Exit => return NextAction::Exit,
+            }
         }
         // generic analyzers
         let range = self.analyzers.generic_range();
         if let Some((start, end)) = range {
-            self.scan_range(content, start, end, depth);
+            match self.scan_range(content, start, end) {
+                NextAction::Continue => {},
+                NextAction::Skip => return NextAction::Continue, // skip current content
+                NextAction::Exit => return NextAction::Exit,
+            }
         }
+        // extractors (specfic)
+        if let Some(ty) = ty {
+            if let Some((start,end)) = self.extractors.range(ty) {
+                match self.extract_range(content, start, end, depth) {
+                    NextAction::Continue => {},
+                    NextAction::Skip => return NextAction::Continue, // skip current content
+                    NextAction::Exit => return NextAction::Exit,
+                }
+            }
+        }
+        // extractors (generc)
+        let range = self.extractors.generic_range();
+        if let Some((start, end)) = range {
+            match self.extract_range(content, start, end, depth) {
+                NextAction::Continue => {},
+                NextAction::Skip => return NextAction::Continue, // skip current content
+                NextAction::Exit => return NextAction::Exit,
+            }
+        }
+        NextAction::Continue
     }
-    fn scan_range(&mut self, content: &mut dyn Content<T>, start: usize, end: usize, depth: u32) {
+    fn scan_range(&mut self, content: &mut dyn Content<T>, start: usize, end: usize) -> NextAction {
         if (end <= start) || (end > self.analyzers.len()) {
-            return;
+            return NextAction::Continue;
         }
         for i in start..end {
             let result = unsafe { self.analyzers.get(i).analyze(content, &mut self.varm) };
             match result {
                 NextAction::Continue => continue,
-                NextAction::Exit => break,
-                NextAction::Skip => break,
+                NextAction::Exit => return NextAction::Exit,
+                NextAction::Skip => return NextAction::Skip,
             }
         }
+        NextAction::Continue
     }
-    fn extract_content(&mut self, content: &mut dyn Content<T>, index: usize, depth: u32) {
-        let len = self.analyzers.len();
-        // if index >= len {
-        //     return;
-        // }
-        // let mut entry = Entry {
-        //     path: String::new(),
-        //     size: Some(0),
-        //     cursor: EntryCursor::Offset(0),
-        // };
-        // let inited = unsafe { self.analyzers.get(index).init_entry(content, &mut entry) };
-        // if !inited {
-        //     return;
-        // }
-        // loop {
-        //     let should_process =
-        //         self.filter
-        //             .should_process(&entry.path, depth + 1, entry.size.unwrap_or(0));
-        //     if should_process {
-        //         if let Some(mut extracted_content) =
-        //             unsafe { self.analyzers.get(index).extract_entry(content, &entry) }
-        //         {
-        //             self.inner_scan(&mut *extracted_content, depth + 1);
-        //         }
-        //     }
-        //     if !unsafe { self.analyzers.get(index).next_entry(content, &mut entry) } {
-        //         break;
-        //     }
-        // }
+    fn extract_range(&mut self, content: &mut dyn Content<T>, start: usize, end: usize, depth: u32) -> NextAction {
+        if (end <= start) || (end > self.analyzers.len()) {
+            return NextAction::Continue;
+        }
+        for i in start..end {
+            let result = self.extract_content(content, i, depth);
+            match result {
+                NextAction::Continue => continue,
+                NextAction::Exit => return NextAction::Exit,
+                NextAction::Skip => return NextAction::Skip,
+            }
+        }
+        NextAction::Continue        
+    }
+    fn extract_content(&mut self, content: &mut dyn Content<T>, index: usize, depth: u32) -> NextAction {
+        let len = self.extractors.len();
+        if index >= len {
+            return NextAction::Continue;
+        }
+        let mut extractor = unsafe { self.extractors.get(index) };
+        if !extractor.init(content, &mut self.varm) {
+            return NextAction::Continue;
+        }
+        while let Some(entry) = unsafe { self.extractors.get(index).advance(content) } {
+            if !self.filter.should_process(&entry.path, depth + 1, entry.size) {
+                continue;
+            }
+            extractor = unsafe { self.extractors.get(index) };
+            if let Some(mut extracted_content) = extractor.extract(content) {
+                let result = self.inner_scan(&mut *extracted_content, depth + 1);
+                match result {
+                    NextAction::Continue => continue,
+                    NextAction::Exit => return NextAction::Exit,
+                    NextAction::Skip => return NextAction::Continue,
+                }
+            }
+        }
+        NextAction::Continue
     }
     fn retrieve_content_type(&self, content: &mut dyn Content<T>) -> Option<T> {
         let p = content.path().as_bytes();
